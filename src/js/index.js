9a93cb9de1ba3cab48802734747c3f250050dab1
amhart02 <har20010@byui.edu>
2025-07-05 23:32:37 -0600

    Started adding functions to search up a city to use for api

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,23 @@
 import { loadHeader, loadFooter } from "./utils.mjs";
+import { cities } from "./cities.mjs";
+
+const menu = document.getElementById("cityMenu");
+
+function loadDropDownMenu (cities) {
+    cities.forEach(city => addMenuItem(city));
+}
+
+function addMenuItem(city) {
+    const html = `<option value="${city.coordinates}">${city.name}</option>`;
+    menu.insertAdjacentHTML("beforeend", html);
+}
+
+function addDestinationsByCoordinates(coordinates) {
+    //grab the coordinates value from drop down menu choice and use the api to display results
+};
 
 loadHeader();
 loadFooter();
+loadDropDownMenu(cities);
+
+menu.addEventListener("change", addDestinationsByCoordinates);
