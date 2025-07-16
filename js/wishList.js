18bae6155022a7c3d895b0c070d4f8cef425e276
amhart02 <har20010@byui.edu>
2025-07-16 00:38:46 -0600

    Fixed file structure

--- js/wishList.js
+++ js/wishList.js
@@ -1,3 +1,4 @@
+import { getGameDetails } from "./api.mjs";
 import { loadHeader, loadFooter, scrollToTop } from "./utils.mjs"
 let wishList = JSON.parse(localStorage.getItem('wishlist')) || [];
 let wishListContainer = document.querySelector(".wishList");
@@ -22,7 +23,34 @@ function initWishList () {
         });
 
         wishListContainer.appendChild(div);
+
+        const viewDetailsButton = div.querySelector(".viewDetails");
+        viewDetailsButton.addEventListener("click", async () => {
+            const gameDetails = await getGameDetails(game.id);
+            renderDialog(game, gameDetails);
+        });
     });
+}
+
+function renderDialog(game, gameDetails) {
+    const dialog = document.querySelector(".gameDialog");
+    const dialogContent = dialog.querySelector(".dialog-content");
+
+    dialogContent.innerHTML = `
+    <h2>${game.name}</h2>
+    <img src="${game.background_image}" alt="${game.name}"/>
+    <p>${gameDetails.description_raw}</p>
+    <h3>Rating: <span>${game.rating}</span></h3>
+    `;
+
+    const closeButton = dialog.querySelector("#closeButton")
+
+    if (!dialog.open) {
+        dialog.showModal();
+    }
+    closeButton.addEventListener("click", () => {
+        dialog.close();
+    })
 }
 
 function removeGame(game) {
@@ -37,11 +65,12 @@ function gameTemplate(game) {
     return `
             <h2>${game.name}</h2>
             <img src="${game.background_image}" alt="${game.name}" />
-            <h3 class="remove-btn">Remove</h3>
+            <h3 class="viewDetails">View Details</h3>
+            <h5 class="remove-btn">Remove</h5>
     `;
 }
 
 loadFooter();
 loadHeader();
-initWishList();
+await initWishList();
 scrollToTop();
